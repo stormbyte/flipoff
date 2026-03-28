3e58a4431f2f657c88b471042881ebbda84ff356
Your Name <you@example.com>
2026-03-28 20:16:56 +0100

    nginx docker image

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,5 +1,5 @@
 export const GRID_COLS = 22;
-export const GRID_ROWS = 5;
+export const GRID_ROWS = 7;
 
 export const SCRAMBLE_DURATION = 800;
 export const FLIP_DURATION = 300;
@@ -10,17 +10,25 @@ export const MESSAGE_INTERVAL = 4000;
 export const CHARSET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,-!?\'/: ';
 
 export const SCRAMBLE_COLORS = [
-  '#00AAFF', '#00FFCC', '#AA00FF',
-  '#FF2D00', '#FFCC00', '#FFFFFF'
+  '#00AAFF', 
+  '#00FFCC', 
+  '#AA00FF',
+  '#FF2D00', 
+  '#FFCC00', 
+  '#FFFFFF'
 ];
 
 export const ACCENT_COLORS = [
-  '#00FF7F', '#FF4D00', '#AA00FF',
-  '#00AAFF', '#00FFCC'
+  '#00FF7F', 
+  '#FF4D00', 
+  '#AA00FF',
+  '#00AAFF', 
+  '#00FFCC'
 ];
 
 export const MESSAGES = [
   [
+    '',
     '',
     'GOD IS IN',
     'THE DETAILS .',
@@ -29,37 +37,10 @@ export const MESSAGES = [
   ],
   [
     '',
-    'STAY HUNGRY',
-    'STAY FOOLISH',
-    '- STEVE JOBS',
-    ''
-  ],
-  [
     '',
     'GOOD DESIGN IS',
     'GOOD BUSINESS',
     '- THOMAS WATSON',
-    ''
-  ],
-  [
-    '',
-    'LESS IS MORE',
-    '',
-    '- MIES VAN DER ROHE',
-    ''
-  ],
-  [
-    '',
-    'MAKE IT SIMPLE',
-    'BUT SIGNIFICANT',
-    '- DON DRAPER',
-    ''
-  ],
-  [
-    '',
-    'HAVE NO FEAR OF',
-    'PERFECTION',
-    '- SALVADOR DALI',
     ''
   ]
 ];
